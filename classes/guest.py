fcf8142ee267a901e8f875a2e04e0ebcffac1c72
madgelackie <marionmlackie@gmail.com>
2021-09-18 10:20:35 +0100

    room payment progress

--- classes/guest.py
+++ classes/guest.py
@@ -4,3 +4,10 @@ class Guest:
         self.name = input_name
         self.wallet = input_wallet
         self.favourite_song = input_favourite_song
+
+    def pay_for_room(self, room):
+        if self.wallet >= room.fee:
+            self.wallet -= room.fee
+        return "Sorry, unable to take payment"
+
+
--- classes/room.py
+++ classes/room.py
@@ -1,10 +1,12 @@
 class Room:
 
-    def __init__(self, input_room_name, input_capacity):
+    def __init__(self, input_room_name, input_capacity, input_fee):
         self.room_name = input_room_name
         self.capacity = input_capacity
         self.guest_list = []
         self.playlist = []
+        self.fee = input_fee
+        self.takings = 0
 
     def add_to_playlist(self, song):
         self.playlist.append(song)
@@ -22,8 +24,12 @@ class Room:
     def guest_list_length(self):
         return len(self.guest_list)
 
+    def take_payment(self, guest):
+        # guest.pay_for_room(self, room)
+        self.takings += self.fee
+# how to charge customer, increase takings, ?need to include reduce customer wallet
 
-
+ # guest.pay_for_room(self, room)
 
 
 
